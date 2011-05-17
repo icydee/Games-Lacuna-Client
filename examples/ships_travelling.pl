f77ca6bc433f8bbd8ff7557ed96c1492615b52ef
fireartist <fireartist@gmail.com>
2011-05-17 11:23:43 +0100

    allow multiple --planet opts

--- examples/ships_travelling.pl
+++ examples/ships_travelling.pl
@@ -10,11 +10,11 @@ use Date::Format;
 use List::Util (qw(first));
 use Getopt::Long          (qw(GetOptions));
 
-my $planet_name;
+my @planets;
 my $ships_per_page = 25;
 
 GetOptions(
-    'planet=s' => \$planet_name,
+    'planet=s' => \@planets,
 );
 
 my $cfg_file = shift(@ARGV) || 'lacuna.yml';
@@ -42,12 +42,13 @@ my $client = Games::Lacuna::Client->new(
 my $empire  = $client->empire->get_status->{empire};
 
 # reverse hash, to key by name instead of id
-my %planets = reverse %{ $empire->{planets} };
+my %planets = map { $empire->{planets}{$_}, $_ } keys %{ $empire->{planets} };
 
 my @spaceports;
 
 foreach my $name ( sort keys %planets ) {
-  next if defined $planet_name && $planet_name ne $name;
+  
+  next if @planets && none { $name eq $_ } @planets;
   
   # Load planet data
   my $planet    = $client->body( id => $planets{$name} );
